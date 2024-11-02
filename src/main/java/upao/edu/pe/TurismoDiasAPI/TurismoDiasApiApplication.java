0283faaf2e61f89fc9953ce195236a601885841c
Frankcb04 <143514921+Frankcb04@users.noreply.github.com>
2024-11-01 20:09:29 -0500

    TurismoDiasAPI V1.0
    Versión 1.0 del API para el aplicativo móvil de Turismo Dias

---
+++ src/main/java/upao/edu/pe/TurismoDiasAPI/TurismoDiasApiApplication.java
@@ -0,0 +1,13 @@
+package upao.edu.pe.TurismoDiasAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurismoDiasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurismoDiasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/upao/edu/pe/TurismoDiasAPI/TurismoDiasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package upao.edu.pe.TurismoDiasAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurismoDiasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
